ecdf947951d8f2650a6555315eba6a9d2e8e8be7
Angelo Guiao <angeloguiao.gci@gmail.com>
2025-01-05 17:15:08 +0800

    tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <a href='www.google.com'>Try</a>
     </div>
   );
 }
